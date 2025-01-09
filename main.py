a11f557af342f3d95a87d0b2e8c33a3221dc3724
hkourkchi <hkourkchi@yahoo.com>
2025-01-08 21:30:34 -0500

    PRJ-1: Added batch conversion from csv files

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from pyproj import Transformer
 from pyproj import CRS
+import pandas as pd
+import numpy as np
+import os
 
 def convert_nad83_to_wgs84(nad83_easting, nad83_northing):
     """
@@ -27,8 +30,30 @@ def convert_nad83_to_wgs84(nad83_easting, nad83_northing):
 if __name__ == '__main__':
     nad83_easting = 305517.50  # Example easting in NAD83 MTM Zone 8
     nad83_northing = 5036303.10  # Example northing in NAD83 MTM Zone 8
-    wgs84_coordinates = convert_nad83_to_wgs84(nad83_easting, nad83_northing)
-    print(f"NAD83 Coordinates: Easting = {nad83_easting}, Northing = {nad83_northing}")
-    print(f"WGS84 Coordinates: Longitude = {wgs84_coordinates[0]}, Latitude = {wgs84_coordinates[1]}")
+    # # Load the CSV file
+    file_path_in = 'input_nad83.csv'
+    file_data_in = pd.read_csv(file_path_in)
+    print(file_data_in.head())
 
+    file_data_out = pd.DataFrame(np.zeros(file_data_in.shape), columns=['wgs84_longitude', 'wgs84_latitude'])
+    for index, row in file_data_in.iterrows():
+        # longitude, latitude = convert_nad83_to_wgs84(row.nad83_easting, row.nad83_northing)
 
+        file_data_out.iloc[index] = convert_nad83_to_wgs84(row.nad83_easting, row.nad83_northing)
+    print(file_data_out.head())
+
+    file_path_out = 'output_wgs84.csv'
+
+    # Check if the file already exists
+    if os.path.exists(file_path_out):
+        overwrite = input(f"The file {file_path_out} already exists. Do you want to overwrite it? (y/n): ")
+        if overwrite.lower() == 'y':
+            # Save the DataFrame to a CSV file
+            file_data_out.to_csv(file_path_out, index=False)
+            print(f"The file has been overwritten and saved to {file_path_out}.")
+        else:
+            print("The file was not overwritten.")
+    else:
+        # Save the DataFrame to a CSV file
+        file_data_out.to_csv(file_path_out, index=False)
+        print(f"The file has been saved to {file_path_out}.")
