acb2edc86b8e1f5baa5e4885490244fc28c31af9
hkourkchi <hkourkchi@yahoo.com>
2025-01-14 19:36:00 -0500

    Addressed plint warnings

---
+++ convert_nad83_to_wgs_84_unit_test.py
@@ -0,0 +1,42 @@
+"""
+This is a unit test for convert_nad83_to_wgs84 function
+"""
+
+import unittest
+import pandas as pd
+from main import convert_nad83_to_wgs84
+
+
+class ConvertNad83ToWgs84UnitTest(unittest.TestCase):
+    """
+    This is a unit test for convert_nad83_to_wgs84 function
+    """
+    def test_convert_nad83_to_wgs84_precision_place_5(self):
+        """
+        Verifies the output precision up to 5 decimal places
+        """
+        file_path_validation = 'validation_nad83_wgs84.csv'
+        df_val = pd.read_csv(file_path_validation)
+        for index, row in df_val.iterrows():
+            long_out, lat_out = convert_nad83_to_wgs84(row.nad83_easting, row.nad83_northing)
+            self.assertAlmostEqual(long_out, row.wgs84_longitude, 5,
+                                   f"Error in longitude conversion in row : {index}")
+            self.assertAlmostEqual(lat_out, row.wgs84_latitude, 5,
+                                   f"Mismatch in latitude conversion in row : {index}")
+
+    def test_convert_nad83_to_wgs84_precision_place_6(self):
+        """
+        Verifies the output precision up to 6 decimal places
+        """
+        file_path_validation = 'validation_nad83_wgs84.csv'
+        df_val = pd.read_csv(file_path_validation)
+        for index, row in df_val.iterrows():
+            long_out, lat_out = convert_nad83_to_wgs84(row.nad83_easting, row.nad83_northing)
+            self.assertAlmostEqual(long_out, row.wgs84_longitude, 6,
+                                   f"Error in longitude conversion in row : {index}")
+            self.assertAlmostEqual(lat_out, row.wgs84_latitude, 6,
+                                   f"Mismatch in latitude conversion in row : {index}")
+
+
+if __name__ == '__main__':
+    unittest.main()
--- main.py
+++ main.py
@@ -1,10 +1,35 @@
+"""
+This script converts coordinates from the NAD83 MTM Zone 8 projection system to the WGS84 system.
+The script reads input coordinates from a CSV file, performs the transformation, and saves the
+output as a new CSV file.
+
+The script uses the `pyproj` library for coordinate transformation and `pandas` for CSV file
+handling.
+It also includes safeguards to prevent overwriting the output file unless explicitly allowed by the
+user.
+
+Usage:
+    1. Prepare an input CSV file named `input_nad83.csv` with columns `nad83_easting` and
+    `nad83_northing`.
+    2. Run the script to generate an output CSV file named `output_wgs84.csv` containing
+       `wgs84_longitude` and `wgs84_latitude`.
+    3. If the output file already exists, the user will be prompted for confirmation before
+    overwriting.
+
+Requirements:
+    - pyproj
+    - pandas
+    - numpy
+"""
+
+import os
 from pyproj import Transformer
 from pyproj import CRS
 import pandas as pd
 import numpy as np
-import os
 
-def convert_nad83_to_wgs84(nad83_easting, nad83_northing):
+
+def convert_nad83_to_wgs84(nad83_easting_in, nad83_northing_in):
     """
     Convert coordinates from NAD83 MTM Zone 8 to WGS84.
 
@@ -15,6 +40,7 @@ def convert_nad83_to_wgs84(nad83_easting, nad83_northing):
     Returns:
     tuple: (longitude, latitude) in WGS84
     """
+
     nad83_mtm = CRS.from_string("EPSG:2950")
     wgs84 = CRS.from_string("EPSG:4326")
 
@@ -22,38 +48,39 @@ def convert_nad83_to_wgs84(nad83_easting, nad83_northing):
     transformer = Transformer.from_crs(nad83_mtm, wgs84, always_xy=True)
 
     # Perform the transformation to WGS84
-    longitude, latitude = transformer.transform(nad83_easting, nad83_northing)
+    longitude, latitude = transformer.transform(nad83_easting_in, nad83_northing_in)
 
     return longitude, latitude
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    nad83_easting = 305517.50  # Example easting in NAD83 MTM Zone 8
-    nad83_northing = 5036303.10  # Example northing in NAD83 MTM Zone 8
     # # Load the CSV file
-    file_path_in = 'input_nad83.csv'
-    file_data_in = pd.read_csv(file_path_in)
+    FILE_PATH_IN = 'input_nad83.csv'
+    file_data_in = pd.read_csv(FILE_PATH_IN)
     print(file_data_in.head())
 
-    file_data_out = pd.DataFrame(np.zeros(file_data_in.shape), columns=['wgs84_longitude', 'wgs84_latitude'])
+    file_data_out = pd.DataFrame(np.zeros(file_data_in.shape),
+                                 columns=['wgs84_longitude', 'wgs84_latitude'])
     for index, row in file_data_in.iterrows():
         # longitude, latitude = convert_nad83_to_wgs84(row.nad83_easting, row.nad83_northing)
 
         file_data_out.iloc[index] = convert_nad83_to_wgs84(row.nad83_easting, row.nad83_northing)
     print(file_data_out.head())
 
-    file_path_out = 'output_wgs84.csv'
+    FILE_PATH_OUT = 'output_wgs84.csv'
 
     # Check if the file already exists
-    if os.path.exists(file_path_out):
-        overwrite = input(f"The file {file_path_out} already exists. Do you want to overwrite it? (y/n): ")
+    if os.path.exists(FILE_PATH_OUT):
+        overwrite = input(
+            f"The file {FILE_PATH_OUT} already exists. Do you want to overwrite it? (y/n): ")
         if overwrite.lower() == 'y':
             # Save the DataFrame to a CSV file
-            file_data_out.to_csv(file_path_out, index=False)
-            print(f"The file has been overwritten and saved to {file_path_out}.")
+            file_data_out.to_csv(FILE_PATH_OUT, index=False)
+            print(f"The file has been overwritten and saved to {FILE_PATH_OUT}.")
         else:
             print("The file was not overwritten.")
     else:
         # Save the DataFrame to a CSV file
-        file_data_out.to_csv(file_path_out, index=False)
-        print(f"The file has been saved to {file_path_out}.")
+        file_data_out.to_csv(FILE_PATH_OUT, index=False)
+        print(f"The file has been saved to {FILE_PATH_OUT}.")
